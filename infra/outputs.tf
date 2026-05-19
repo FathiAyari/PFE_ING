d2df3a78b27c7a254d98ddb272b643138a3335c7
fathi <fathi@pfe.local>
2026-05-20 00:57:04 +0100

    infra: roll back to VM-based design (VM will be created manually in Azure portal)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,26 +2,26 @@ output "resource_group" {
   value = azurerm_resource_group.rg.name
 }
 
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "vm_size" {
+  value = azurerm_linux_virtual_machine.vm.size
+}
+
+output "vm_location" {
+  value = azurerm_linux_virtual_machine.vm.location
+}
+
 output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
-}
-
-output "aci_name" {
-  value = azurerm_container_group.app.name
-}
-
-output "aci_public_ip" {
-  value = azurerm_container_group.app.ip_address
-}
-
-output "aci_fqdn" {
-  value = azurerm_container_group.app.fqdn
-}
-
-output "aci_location" {
-  value = azurerm_container_group.app.location
 }
